dd4a4fed9a36ee8d263ee35cd218fbf3b776c176
Alexlmth <alex.kungs@gmail.com>
2026-09-05 12:03:41 +0300

    4.17. Шаблонизируй то

---
+++ src/view/no-point-view.js
@@ -0,0 +1,22 @@
+import AbstractView from '../framework/view/abstract-view.js';
+import { NoPointTextType } from '../const.js';
+
+function createNoPointTemplate(filterType) {
+  const noPointText = NoPointTextType[filterType];
+
+  return `<p class="trip-events__msg">${noPointText}</p>`;
+}
+
+export default class NoPointView extends AbstractView {
+  #filterType = null;
+
+  constructor({ filterType }) {
+    super();
+    this.#filterType = filterType;
+  }
+
+  get template() {
+    return createNoPointTemplate(this.#filterType);
+  }
+
+}
